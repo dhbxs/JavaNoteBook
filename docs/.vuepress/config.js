f4829c1cb096c47597b48d50a9e918a3ee714b7f
dhbxs <1723544366@qq.com>
2023-03-05 23:28:57 +0800

    git init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Java基础',
+    description: 'Java基础',
+    // configureWebpack: {
+    //     resolve: {
+    //         alias: {
+    //             '@alias': 'docs/images'
+    //         }
+    //     }
+    // }
+}
